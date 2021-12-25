354b425ff908faaa69dae379d67081ed8efd93e0
JG-11 <80846488+JeeveshGarg@users.noreply.github.com>
2021-12-25 21:24:57 +0530

    add models app catalog

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from catalog.models import *
 # Register your models here.
+
+admin.site.register(Book)
+admin.site.register(BookCopy)
+admin.site.register(BookRating)
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,40 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    description = models.TextField(null=True)
+    maximum_retail_price = models.PositiveIntegerField()
+    rating = models.FloatField(default=0.0)
+
+    class Meta:
+        ordering = ('title',)
+
+    def __str__(self):
+        return f'{self.title} by {self.author}'
+
+
+class BookCopy(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    borrow_date = models.DateField(null=True, blank=True)
+    # True status means that the copy is available for issue, False means unavailable
+    status = models.BooleanField(default=False)
+    borrower = models.ForeignKey(User, related_name='borrower', null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        if self.borrow_date:
+            return f'{self.book.title}, {str(self.borrow_date)}'
+        else:
+            return f'{self.book.title} - Available'
+
+class BookRating(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    rater = models.ForeignKey(User, related_name='rater', null=True, blank=True, on_delete=models.SET_NULL)
+    rated = models.BooleanField(default = False)
+    rating=models.FloatField(default=0.0)
+
+    def __str__(self):
+        return f'{self.book.title} by {self.rater} : {self.rated}'
